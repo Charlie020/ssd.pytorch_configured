e7086b76fde1b11bf3a39c6fe8c8a4bc8266dd97
25803 <2580390720@qq.com>
2023-06-26 23:35:40 +0800

    修改了clear.py

--- data/VOCdevkit/VOC2007/clear.py
+++ data/VOCdevkit/VOC2007/clear.py
@@ -6,7 +6,7 @@ XmlFilePath = 'Annotations/'
 xmlFileName = os.listdir(XmlFilePath)
 xmlNum = len(xmlFileName)
 
-JpgFilePath = 'JPEGImages/'
+JpgFilePath = 'labels/'
 jpgFileName = os.listdir(JpgFilePath)
 jpgNum = len(jpgFileName)
 
@@ -44,3 +44,21 @@ elif xmlNum < jpgNum:          # 图片数量更多
 else:
     print("图片与标签数量相等。")
 
+# with open("ImageSets/Main/test.txt", "r") as file:
+#     lines = file.readlines()
+#
+# lines = [line for line in lines if line.strip() not in deleteList]
+#
+# with open("ImageSets/Main/test.txt", "w") as file:
+#     file.writelines(lines)
+
+# 定义文件夹路径
+# folder_path = 'labels'
+# # 遍历文件夹
+# for filename in os.listdir(folder_path):
+#     # 获取文件的完整路径
+#     file_path = os.path.join(folder_path, filename)
+#
+#     # 检查文件名是否为 '1'，如果是则删除文件
+#     if filename in deleteList:
+#         os.remove(file_path)
